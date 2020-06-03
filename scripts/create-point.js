46256767e46245d5be67bbee6be0894d7e3bef08
Pedro Hugo <pedrohugo.ov09@gmail.com>
2020-06-03 07:51:04 -0300

    Create Point page created

---
+++ scripts/create-point.js
@@ -0,0 +1,35 @@
+const populateStates = () => {
+    const stateSelect = document.querySelector("select[name=state]");
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(response => response.json())
+        .then(states => {
+            for(state of states) {
+                stateSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+            }
+        })
+        .catch(error => console.log(error))
+}
+
+populateStates();
+
+const getCities = (event) => {
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("input[name=uf]");
+    const indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+    console.log(event.target.options[indexOfSelectedState]);
+    const state = event.target.value;
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`
+    fetch(url)
+        .then(response => response.json())
+        .then(cities => {
+            for(city of cities){
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+            }
+            citySelect.disabled = false;
+        })
+        .catch(error => console.log(error))
+}
+
+document.querySelector("select[name=state]")
+        .addEventListener("change", getCities)
